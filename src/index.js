22eb134218100991db0f68dee3d7894d375707ac
Sadman Sakib <34483142+sadmansakib811@users.noreply.github.com>
2022-11-20 21:57:34 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <img
+      className="coverpic"
+      src="https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000"
+      alt="BigCo Inc. logo"
+    />
+
+    <h1 className="heading">My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+
+    <img
+      className="img1"
+      src="https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000"
+      alt="BigCo Inc. logo"
+    />
+    <img
+      className="img2"
+      src="https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000"
+      alt="BigCo Inc. logo"
+    />
+    <img
+      className="img3"
+      src="https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000"
+      alt="BigCo Inc. logo"
+    />
+  </div>,
+  document.getElementById("root")
+);
